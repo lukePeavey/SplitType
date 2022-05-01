d9ca2fd435f0a175f7f5c234e73916099e1d3d00
Luke Peavey <1williampeavey@gmail.com>
2022-05-01 19:46:34 -0400

    chore: fix tests

--- __tests__/utils/parseSettings.test.js
+++ __tests__/utils/parseSettings.test.js
@@ -2,6 +2,6 @@ import parseSettings from '../../lib/utils/parseSettings'
 
 describe(`utils.parseSettings`, () => {
   it(`Accepts \`split\` as an alias for \`types\``, () => {
-    expect(parseSettings({ split: 'words' }).types).toEqual('words')
+    expect(parseSettings({ split: 'words' }).types).toEqual(['words'])
   })
 })

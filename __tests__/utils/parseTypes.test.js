ee10805f49878d08c04a1c68296a231f3889460d
Luke Peavey <1williampeavey@gmail.com>
2022-05-11 22:55:17 -0400

    Small performance improvement

--- __tests__/utils/parseTypes.test.js
+++ __tests__/utils/parseTypes.test.js
@@ -15,19 +15,28 @@ describe('utils.parseTypes(value)', () => {
   it(`The value of each key is \`true\` if the given string contains 
   that key`, () => {
     expect(parseTypes('lines, words')).toEqual({
+      none: false,
       lines: true,
       words: true,
       chars: false,
     })
     expect(parseTypes('lines')).toEqual({
+      none: false,
       lines: true,
       words: false,
       chars: false,
     })
     expect(parseTypes('lines, chars')).toEqual({
+      none: false,
       lines: true,
       words: false,
       chars: true,
+    })
+    expect(parseTypes([])).toEqual({
+      none: true,
+      lines: false,
+      words: false,
+      chars: false,
     })
   })
 })

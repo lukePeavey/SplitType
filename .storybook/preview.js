6050c0c3ccffb39d61b70e8a6464ac1995088dfe
Luke Peavey <8286271+lukePeavey@users.noreply.github.com>
2022-06-13 00:28:36 -0400

    Add docs to storybook (#35)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,14 @@
-export const parameters = {}
+import { themes } from '@storybook/theming'
+import theme from './theme'
+
+// or global addParameters
+export const parameters = {
+  docs: {
+    theme: theme,
+  },
+  previewTabs: {
+    'storybook/docs/panel': {
+      hidden: true,
+    },
+  },
+}

492458475f78d81de6c2c0dabbefae00b2529290
Luke Peavey <8286271+lukePeavey@users.noreply.github.com>
2022-07-03 16:42:03 -0400

    Fix potential memory leak issue (#41)

--- __tests__/utils/Data.test.js
+++ __tests__/utils/Data.test.js
@@ -1,7 +1,54 @@
-import { Data, removeData } from '../../lib/Data'
+import * as data from '../../lib/Data'
+
+const owner = document.createElement('div')
+
+afterEach(() => {
+  data.remove(owner)
+})
 
 describe(`Data(owner)`, () => {
-  it(`Returns empty object when owner is undefined`, () => {
-    expect(Data(undefined)).toEqual({})
+  it(`data.get(undefined) returns empty object`, () => {
+    expect(data.get(undefined)).toEqual({})
+  })
+  it(`data.set(owner, key, value) works as expected`, () => {
+    // Make sure data store is empty at start of test
+    expect(data.cache).toEqual({})
+    data.set(owner, 'foo', 'bar')
+    expect(data.cache[owner[data.expando]]).toEqual({ foo: 'bar' })
+    data.set(owner, 'hello', 'world')
+    expect(data.cache[owner[data.expando]]).toEqual({
+      foo: 'bar',
+      hello: 'world',
+    })
+  })
+  it(`data.set(owner, objectMap) works as expected`, () => {
+    // Make sure data store is empty at start of test
+    expect(data.cache).toEqual({})
+    data.set(owner, {
+      foo: 'bar',
+      hello: 'world',
+    })
+    expect(data.cache[owner[data.expando]]).toEqual({
+      foo: 'bar',
+      hello: 'world',
+    })
+  })
+
+  it(`data.get(owner) returns data object `, () => {
+    // Make sure data store is empty at start of test
+    expect(data.get(owner)).toEqual({})
+    // Set multiple properties by passing an object as the second arg
+    data.set(owner, {
+      foo: 'bar',
+      hello: 'world',
+    })
+    // Set an additional property using the (owner, key, value) syntax
+    data.set(owner, 'other', 'value')
+    // data.get(owner) should return an object with the the expected props
+    expect(data.get(owner)).toEqual({
+      foo: 'bar',
+      hello: 'world',
+      other: 'value',
+    })
   })
 })

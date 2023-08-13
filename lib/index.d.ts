3ab6b1a726f4cf009cddf7ea6e9ba12c8f7f42ae
Steven John <stevenjpx2@gmail.com>
2023-08-13 21:40:23 +0530

    feat: simplified typing for type options

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,30 +1,4 @@
 declare module 'split-type' {
-  /**
-   * UnionToIntersection<{ foo: string } | { bar: string }> =
-   *  { foo: string } & { bar: string }.
-   */
-  type UnionToIntersection<U> = (
-    U extends unknown ? (arg: U) => 0 : never
-  ) extends (arg: infer I) => 0
-    ? I
-    : never
-
-  /**
-   * LastInUnion<1 | 2> = 2.
-   */
-  type LastInUnion<U> = UnionToIntersection<
-    U extends unknown ? (x: U) => 0 : never
-  > extends (x: infer L) => 0
-    ? L
-    : never
-
-  /**
-   * UnionToTuple<1 | 2> = [1, 2].
-   */
-  type UnionToTuple<U, Last = LastInUnion<U>> = [U] extends [never]
-    ? []
-    : [...UnionToTuple<Exclude<U, Last>>, Last]
-
   type StringedCombination<
     T extends string[],
     Sep extends string = ' ',
@@ -34,21 +8,11 @@ declare module 'split-type' {
     ? Item | `${Item}${Sep}${StringedCombination<[], Sep, Exclude<All, Item>>}`
     : never
 
-  type TupledCombination<
-    T extends string[],
-    All = T[number],
-    Item = All,
-  > = Item extends string
-    ? [Item] | [Item, ...TupledCombination<[], Exclude<All, Item>>]
-    : never
+  type TypesValue = ['lines', 'words', 'chars']
 
-  type TypesValue = 'lines' | 'words' | 'chars'
-  type TupleTypesValue = UnionToTuple<TypesValue>
+  type TypesListString = StringedCombination<TypesValue, ','>
 
-  type TypesListString = StringedCombination<TupleTypesValue, ','>
-  type TypesListTuple = TupledCombination<TupleTypesValue>
-
-  type TypesList = TypesListString | TypesValue[]
+  type TypesList = TypesListString | TypesValue[number][]
 
   type SplitTypeOptions = {
     absolute: boolean

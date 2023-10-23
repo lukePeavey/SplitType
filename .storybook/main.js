e2495a3e6bf58341f8de45a71d98e008dc867638
Luke Peavey <8286271+lukePeavey@users.noreply.github.com>
2023-10-22 23:36:19 -0400

    Upgrade to storybook 7
    - Migrate from storybook 6 to 7
    - switch from yarn to npm

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,21 @@ module.exports = {
     '../__stories__/**/*.stories.mdx',
     '../__stories__/**/*.stories.js',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-mdx-gfm'
+  ],
+
   staticDirs: ['../__stories__/assets'],
+
+  framework: {
+    name: '@storybook/svelte-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
